e822abc7d54d7c9a0507323ec0d94aac324cfec1
LBartolini <l.bartolini02@gmail.com>
2019-06-29 10:42:05 +0200

    changed the way the explorer works, still some bugs not reaching the real minimum of the function, added some parameters to control the process

--- gd.py
+++ gd.py
@@ -3,7 +3,7 @@ import numpy as np
 import random as r
 
 
-def gradientDescent(funct, data_to_display=np.arange(-5, 5, 0.001), precision_points=3, lr=1e-2, display=False, steps_to_display=300, logspace_limits=(-1, 0), explorer_reduction=10, precision_tangent=1e-4, treshold_to_stop=1e3):
+def gradientDescent(funct, data_to_display=np.arange(-5, 5, 0.001), precision_points=3, lr=1e-2, display=False, steps_to_display=300, logspace_limits=(-1, 0), explorer_reduction=10, precision_tangent=1e-4, treshold_to_stop=1e3, base_offset=0.5, explorer_importance=2):
 	minimum = None
 	if precision_points < 3:
 		precision_points = 3
@@ -15,6 +15,7 @@ def gradientDescent(funct, data_to_display=np.arange(-5, 5, 0.001), precision_po
 		plt.scatter(points, funct(points), color='red')
 		plt.show()
 	yp_points = [5 for _ in range(precision_points)]
+	y_min = 1e1000
 	step = 0
 	min_value = 1
 
@@ -23,21 +24,21 @@ def gradientDescent(funct, data_to_display=np.arange(-5, 5, 0.001), precision_po
 		yp_points = deriv(funct, points)
 		minimum = points[np.argmin(np.abs(yp_points))]
 		min_value = yp_points[np.argmin(np.abs(yp_points))]
-		diff = min_value*lr*np.abs(min_value) #da cancellare
 		if min_value>0:
 			points = [minimum-np.abs(lr*i*np.abs(min_value)) for i in np.logspace(logspace_limits[0], logspace_limits[1], precision_points-2)]
-			points.append(minimum-(np.abs(lr*np.abs(min_value)*(10**logspace_limits[1])/explorer_reduction)))
-			points.append(minimum+(np.abs(lr*np.abs(min_value)*(10**logspace_limits[1])/explorer_reduction)))
 		else:
 			points = [minimum+np.abs(lr*i*np.abs(min_value)) for i in np.logspace(logspace_limits[0], logspace_limits[1], precision_points-2)]
-			points.append(minimum+(np.abs(lr*np.abs(min_value)*(10**logspace_limits[1])/explorer_reduction)))
-			points.append(minimum-(np.abs(lr*np.abs(min_value)*(10**logspace_limits[1])/explorer_reduction)))
+		
+		points.append(minimum+np.abs(lr*(10**logspace_limits[1]))+base_offset+(np.abs(min_value**explorer_importance)/explorer_reduction))
+		points.append(minimum+np.abs(lr*(10**logspace_limits[1]))-base_offset-(np.abs(min_value**explorer_importance)/explorer_reduction))
+		#points.append(minimum-(np.abs(lr*(1/np.abs(min_value))*(10**logspace_limits[1])/explorer_reduction))-base_offset)
 
 		if display and step%steps_to_display==0:
 			plt.plot(data_to_display, curve, c='green')
 			plt.scatter(points, funct(points), color='red')
+			plt.scatter(minimum, funct(minimum), color='blue')
 			plt.show()
-			print(min_value, np.abs(diff)) #da cancellare
+			print(min_value) #da cancellare
 
 	if display:	
 		plt.plot(data_to_display, curve, c='green')
--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from gd import *
 
 def fun(x):
 	x= np.array(x)
-	#return x**2 + np.sin(5*x)
-	return x**2 - 2*x
+	return x**2 + np.sin(5*x)
 
 results = []
 for i in range(10):
-	minim, step = gradientDescent(data_to_display=np.arange(-5, 5, 0.001), funct=fun, lr=1e-2, display=False, precision_points=10, logspace_limits=(-2, 1), steps_to_display=10, explorer_reduction=0.4)
+	minim, step = gradientDescent(data_to_display=np.arange(-5, 5, 0.001), funct=fun, lr=1e-2, display=False, precision_points=10, logspace_limits=(-2, 1), steps_to_display=30, explorer_reduction=2.5, base_offset=0.4, explorer_importance=3, treshold_to_stop=1e1000)
 	results.append(minim)
 	print(f"The minimum of the function is {minim}, reached with {step} step. Epoch {i}")
 
