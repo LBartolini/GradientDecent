0619e6bc57f48b33aa87971e1d7579a430ad5b8a
LorenzoBartolini <LBartolini>
2019-06-30 21:17:56 +0200

    probably final version, actually work pretty well

--- gd.py
+++ gd.py
@@ -4,50 +4,51 @@ import random as r
 
 
 def gradientDescent(funct, data_to_display=np.arange(-5, 5, 0.001), precision_points=3, lr=1e-2, display=False, steps_to_display=300, logspace_limits=(-1, 0), explorer_reduction=10, precision_tangent=1e-4, treshold_to_stop=1e3, base_offset=0.5, explorer_importance=2):
-	minimum = None
+	minimum_x = None
+	minimum_y = 1e1000
 	if precision_points < 3:
 		precision_points = 3
 	points = np.array([r.choice(data_to_display) for _ in range(precision_points)])
-	#points = [1.5 for _ in range(precision_points)]
 	curve = funct(data_to_display)
 	if display:
 		plt.plot(data_to_display, curve, c='green')
 		plt.scatter(points, funct(points), color='red')
 		plt.show()
 	yp_points = [5 for _ in range(precision_points)]
-	y_min = 1e1000
 	step = 0
 	min_value = 1
 
 	while np.abs(min_value) > precision_tangent and step < treshold_to_stop:
 		step += 1
 		yp_points = deriv(funct, points)
-		minimum = points[np.argmin(np.abs(yp_points))]
+		for i in points:
+			if funct(i) < minimum_y:
+				#minimum_x = points[np.argmin(np.abs(yp_points))]
+				minimum_x = i
+				minimum_y = funct(minimum_x)
 		min_value = yp_points[np.argmin(np.abs(yp_points))]
 		if min_value>0:
-			points = [minimum-np.abs(lr*i*np.abs(min_value)) for i in np.logspace(logspace_limits[0], logspace_limits[1], precision_points-2)]
+			points = [minimum_x-np.abs(lr*i*np.abs(min_value)) for i in np.logspace(logspace_limits[0], logspace_limits[1], precision_points-2)]
 		else:
-			points = [minimum+np.abs(lr*i*np.abs(min_value)) for i in np.logspace(logspace_limits[0], logspace_limits[1], precision_points-2)]
+			points = [minimum_x+np.abs(lr*i*np.abs(min_value)) for i in np.logspace(logspace_limits[0], logspace_limits[1], precision_points-2)]
 		
-		points.append(minimum+np.abs(lr*(10**logspace_limits[1]))+base_offset+(np.abs(min_value**explorer_importance)/explorer_reduction))
-		points.append(minimum+np.abs(lr*(10**logspace_limits[1]))-base_offset-(np.abs(min_value**explorer_importance)/explorer_reduction))
-		#points.append(minimum-(np.abs(lr*(1/np.abs(min_value))*(10**logspace_limits[1])/explorer_reduction))-base_offset)
+		points.append(minimum_x+np.abs(lr*(10**logspace_limits[1]))+base_offset+(np.abs(min_value**explorer_importance)/explorer_reduction))
+		points.append(minimum_x+np.abs(lr*(10**logspace_limits[1]))-base_offset-(np.abs(min_value**explorer_importance)/explorer_reduction))
 
 		if display and step%steps_to_display==0:
 			plt.plot(data_to_display, curve, c='green')
 			plt.scatter(points, funct(points), color='red')
-			plt.scatter(minimum, funct(minimum), color='blue')
+			plt.scatter(minimum_x, funct(minimum_x), color='blue')
 			plt.show()
-			print(min_value) #da cancellare
 
 	if display:	
 		plt.plot(data_to_display, curve, c='green')
 		plt.scatter(points, funct(points), color='red')
 		plt.show()
-
-	return minimum, step
+	minimum_y = funct(minimum_x)
+	return minimum_x, minimum_y, step
 
 def deriv(funct, x):
 	h = 1e-5
 	x = np.array(x)
-	return (funct(x+h)-funct(x))/h
+	return (funct(x+h)-funct(x-h))/(2*h)
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from gd import *
 
 def fun(x):
 	x= np.array(x)
-	return x**2 + np.sin(5*x)
+	return x**2 + np.sin(10*x)
 
-results = []
+results_x, results_y = [], []
 for i in range(10):
-	minim, step = gradientDescent(data_to_display=np.arange(-5, 5, 0.001), funct=fun, lr=1e-2, display=False, precision_points=10, logspace_limits=(-2, 1), steps_to_display=30, explorer_reduction=2.5, base_offset=0.4, explorer_importance=3, treshold_to_stop=1e1000)
-	results.append(minim)
-	print(f"The minimum of the function is {minim}, reached with {step} step. Epoch {i}")
+	minim_x, minim_y, step = gradientDescent(data_to_display=np.arange(-5, 5, 0.001), funct=fun, lr=1e-1, display=False, precision_points=5, logspace_limits=(-2, 1), steps_to_display=1, explorer_reduction=2.5, base_offset=0.4, explorer_importance=3, treshold_to_stop=1e1000)
+	results_x.append(minim_x)
+	results_y.append(minim_y)
+	print(f"The minimum of the function is {minim_x} at {minim_y}, reached with {step} step. Epoch {i}")
 
-print(results)
+print(f"The Real Global Minimum is {results_x[np.argmin(results_y)]} at {np.min(results_y)}")
